7a0af8b86beff9806bef4d7a48f0452d5f35f606
Sandy_undefined <08sandysk@gmail.com>
2020-12-08 01:08:43 +0530

    Step 4 basic setup done

---
+++ Paper/static/js/pdf.js
@@ -0,0 +1,9 @@
+var typed = new Typed('#pdftyped', {
+    strings: ["Step 4:<strong>Download PDF</strong>"],
+    backSpeed: 20,
+    typeSpeed: 30,
+    backDelay:500,
+    showCursor:true,
+    cursorChar: "|",
+  	contentType: 'html'
+  });

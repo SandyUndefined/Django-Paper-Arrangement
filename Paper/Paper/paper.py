a25be6103feb05f1c6a2f7007ce80f64230b97b0
Sandy_undefined <08sandysk@gmail.com>
2020-12-08 03:33:52 +0530

    Done

---
+++ Paper/Paper/paper.py
@@ -0,0 +1,79 @@
+import os
+
+from django.conf import settings
+from fpdf import FPDF
+import json
+
+title = ''
+logo_data = ''
+json_data = ''
+text_file = []
+
+
+def values(json_file, txt_file, logo, header):
+    json_data = json_file
+    for i in txt_file:
+        text_file.append(i)
+    logo_data = logo
+    title = header
+    filenames = print_data(json_data)
+    output = data(filenames,logo_data, title)
+
+
+def print_data(json_data):
+    # Json to Dict
+    with open(os.path.join(settings.MEDIA_ROOT, json_data), 'rb') as jf:
+        data_dict = json.load(jf)
+
+    # Sorting Dict and Saving them in List
+    sorted_values = dict(sorted(data_dict.items(), key=lambda item: item[1]))
+    filenames = []
+    for i in sorted_values:
+        filenames.append(f'{i}.txt')
+    return filenames
+
+
+# Convert text file into pdf
+
+class PDF(FPDF):
+    def __init__(self,logo_name,header):
+        super(PDF,self).__init__()
+        self.logo = logo_name
+        self.title = header
+
+    def header(self):
+        self.image(os.path.join(settings.MEDIA_ROOT, self.logo), x=14, y=4, h=18, w=18)
+        self.set_font('Arial', 'B', 21)
+        self.set_text_color(r=5, g=57, b=107)
+        self.text(62, 15, self.title)
+
+    def footer(self):
+        self.set_y(-15)
+        self.set_font('Arial', 'I', 8)
+        self.cell(0, 10, 'Page ' + str(self.page_no()), 0, 0, 'C')
+
+    def section(self, name):
+        self.set_top_margin(25)
+        self.set_font('Arial', 'B', 16)
+        self.cell(0, 6, name[:-4], 0, 0, 'C', 0)
+        self.ln(4)
+
+    def question(self, name):
+        with open(os.path.join(settings.MEDIA_ROOT, name), 'r') as fh:
+            txt = fh.read()
+        self.set_font("Arial", '', size=12)
+        self.multi_cell(0, 6, txt)
+        self.ln()
+
+    def print_paper(self, name):
+        self.add_page()
+        self.section(name)
+        self.question(name)
+
+
+def data(filenames,logo,title):
+    pdf = PDF(logo,title)
+    pdf.set_margins(left=15, top=25, right=15)
+    for x in filenames:
+        pdf.print_paper(x)
+    pdf.output("paper.pdf")
--- Paper/Paper/views.py
+++ Paper/Paper/views.py
@@ -1,25 +1,17 @@
-from django.conf import settings
 from django.core.files.storage import FileSystemStorage
 from django.shortcuts import render, redirect
+from Paper.paper import values
 
 
 def json(request):
     return render(request, 'json.html')
 
-
-json_file = ""
-filesname = []
-logo_file = ""
-header = ""
-
-
 def files(request):
     if request.method == 'POST' and request.FILES['files']:
         myfiles = request.FILES['files']
         fs = FileSystemStorage()
-        filename = fs.save(myfiles.name, myfiles)
-        json_file = filename
-        print(json_file)
+        files = fs.save(myfiles.name, myfiles)
+        request.session['files'] = files
         return render(request, 'files.html')
     else:
         return render(request, 'json.html')
@@ -29,10 +21,11 @@ def multiple_files(request):
     if request.method == 'POST' and request.FILES.getlist('files'):
         myfiles = request.FILES.getlist('files')
         fs = FileSystemStorage()
+        filesname = []
         for i in myfiles:
             filename = fs.save(i.name, i)
             filesname.append(filename)
-        print(filesname)
+        request.session['filename'] = filesname
         return render(request, 'logo.html')
     else:
         return render(request, 'files.html')
@@ -43,10 +36,15 @@ def logo(request):
         header = request.POST.get('header')
         myfiles = request.FILES['files']
         fs = FileSystemStorage()
-        filename = fs.save(myfiles.name, myfiles)
-        logo_file = filename
-        print(logo_file)
-        print(header)
+        logo_file = fs.save(myfiles.name, myfiles)
+        # request.session['logo'] = logo
+        # data retrive
+        json_file = request.session.get('files', None)
+        filesname = request.session.get('filename', None)
+        print(f'This is my logo {logo_file} \n this is my header {header} \n this is my json data {json_file} \n this is my text file {filesname}')
+        output_data = values(json_file,filesname,logo_file,header)
         return render(request, 'download.html')
     else:
         return render(request, 'logo.html')
+
+

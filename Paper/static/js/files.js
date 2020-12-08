f0865556936c14fc980e2d8e6ff90a70d0defe10
Sandy_undefined <08sandysk@gmail.com>
2020-12-08 13:37:24 +0530

    added margin and underline

--- Paper/static/js/files.js
+++ Paper/static/js/files.js
@@ -1,5 +1,5 @@
 var typed = new Typed('#filestyped', {
-    strings: ["Step 2:<strong>Upload Multiple Files</strong>"],
+    strings: ["Step 2:<strong>Upload Multiple Text Files</strong>"],
     backSpeed: 20,
     typeSpeed: 30,
     backDelay:500,
